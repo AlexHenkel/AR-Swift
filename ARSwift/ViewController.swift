59c973fd89a5bd0cf32448ed2cede57b007c658f
Alejandro Henkel <alehenkel17@gmail.com>
2019-04-08 23:05:34 -0500

    finish app

--- ARSwift/ViewController.swift
+++ ARSwift/ViewController.swift
@@ -12,14 +12,133 @@ import ARKit
 import Vision
 
 class ViewController: UIViewController, ARSCNViewDelegate {
-
+    
+    @IBOutlet weak var rocketButton: UIButton!
+    @IBOutlet weak var stylesControl: UISegmentedControl!
     @IBOutlet weak var sceneView: ARSCNView!
+    
+    var stylesNumber = "1"
+    var models = [wave_style().model,mosaic_style().model,udnie_style().model] as [MLModel]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        // Show statistics such as fps and timing information
+        sceneView.showsStatistics = true
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+        
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+    
+    @IBAction func handleRocketButton(_ sender: UIButton) {
+        // Create a new scene
+        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        
+        // Set the scene to the view
+        sceneView.scene = scene
+        
+        // Hide button
+        rocketButton.isHidden = true
+    }
+    
+    @IBAction func handleTap(_ sender: UITapGestureRecognizer) {
+        // grab the current AR session frame from the scene, if possible
+        guard let currentFrame = sceneView.session.currentFrame else {
+            return
+        }
+        
+        // setup some geometry for a simple plane
+        let imagePlane = SCNPlane(width:sceneView.bounds.width/6000,
+                                  height:sceneView.bounds.height/6000)
+        
+        // Take a snapshot
+        let startImage = sceneView.snapshot()
+        
+        let newImage = stylizeImage(cgImage: startImage.cgImage!, model: models[Int(stylesNumber)! - 1])
+        
+        imagePlane.firstMaterial?.diffuse.contents = newImage
+        imagePlane.firstMaterial?.lightingModel = .constant
+        
+        // add the node to the scene
+        let planeNode = SCNNode(geometry:imagePlane)
+        sceneView.scene.rootNode.addChildNode(planeNode)
+        
+        // update the node to be a bit in front of the camera inside the AR session
+        
+        // step one create a translation transform
+        var translation = matrix_identity_float4x4
+        translation.columns.3.z = -0.1
+        
+        // step two, apply translation relative to camera for the node
+        planeNode.simdTransform = matrix_multiply(currentFrame.camera.transform, translation )
+    }
+    
+    @IBAction func handleStyleChange(_ sender: UISegmentedControl) {
+        stylesNumber = stylesControl.titleForSegment(at: sender.selectedSegmentIndex)!
+    }
+    
+    
+    // code from fast style transfer example in iOS app
+    // https://github.com/prisma-ai/torch2coreml/tree/master/example/fast-neural-style/ios
+    private func stylizeImage(cgImage: CGImage, model: MLModel) -> CGImage {
+        let input = StyleTransferInput(input: pixelBuffer(cgImage: cgImage, width: 720, height: 720))
+        let outFeatures = try! model.prediction(from: input)
+        let output = outFeatures.featureValue(for: "outputImage")!.imageBufferValue!
+        CVPixelBufferLockBaseAddress(output, .readOnly)
+        let width = CVPixelBufferGetWidth(output)
+        let height = CVPixelBufferGetHeight(output)
+        let data = CVPixelBufferGetBaseAddress(output)!
+        
+        let outContext = CGContext(data: data,
+                                   width: width,
+                                   height: height,
+                                   bitsPerComponent: 8,
+                                   bytesPerRow: CVPixelBufferGetBytesPerRow(output),
+                                   space: CGColorSpaceCreateDeviceRGB(),
+                                   bitmapInfo: CGImageByteOrderInfo.order32Little.rawValue | CGImageAlphaInfo.noneSkipFirst.rawValue)!
+        let outImage = outContext.makeImage()!
+        CVPixelBufferUnlockBaseAddress(output, .readOnly)
+        
+        return outImage
+    }
+    
+    //https://github.com/prisma-ai/torch2coreml/tree/master/example/fast-neural-style/ios
+    private func pixelBuffer(cgImage: CGImage, width: Int, height: Int) -> CVPixelBuffer {
+        var pixelBuffer: CVPixelBuffer? = nil
+        let status = CVPixelBufferCreate(kCFAllocatorDefault, width, height, kCVPixelFormatType_32BGRA , nil, &pixelBuffer)
+        if status != kCVReturnSuccess {
+            fatalError("Cannot create pixel buffer for image")
+        }
+        
+        CVPixelBufferLockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags.init(rawValue: 0))
+        let data = CVPixelBufferGetBaseAddress(pixelBuffer!)
+        let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
+        let bitmapInfo = CGBitmapInfo(rawValue: CGBitmapInfo.byteOrder32Little.rawValue | CGImageAlphaInfo.noneSkipFirst.rawValue)
+        let context = CGContext(data: data, width: width, height: height, bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer!), space: rgbColorSpace, bitmapInfo: bitmapInfo.rawValue)
+        
+        context?.draw(cgImage, in: CGRect(x: 0, y: 0, width: width, height: height))
+        CVPixelBufferUnlockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+        
+        return pixelBuffer!
+    }
+    
 }
 
