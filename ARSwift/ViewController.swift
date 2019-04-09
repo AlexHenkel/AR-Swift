ebbc6e9079474b7b13bfd43697cd46f5f76a7e4b
Alejandro Henkel <alehenkel17@gmail.com>
2019-04-08 22:47:59 -0500

    initial commit

---
+++ ARSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ARSwift
+//
+//  Created by Alejandro Henkel on 4/8/19.
+//  Copyright © 2019 Alejandro Henkel. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+import Vision
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet weak var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
